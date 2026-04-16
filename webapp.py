1dc02b49d1fa9de4ec45fa317c1d1ebbe8ead84b
Tanmay Kumar Chaki <chakitanmay45@gmail.com>
2026-04-16 16:50:23 +0530

    commit3

--- webapp.py
+++ webapp.py
@@ -3,18 +3,72 @@ import streamlit as st
 
 st.set_page_config(
     page_title='AI-Driven-Market Transformation',
-    layout="wide"
+    layout="wide",
+    initial_sidebar_state="expanded" 
 )
 
 #Title
 st.title("AI-Driven Market Transformation: A Comprehensive Analysis")
 
 #Sidebar
+
+st.sidebar.image("passport pic.jpeg", width=150)
+st.sidebar.markdown("## Profile")
+st.sidebar.markdown(
+    """
+    <div style = "text-align: left;">
+        <h3>Tanmay Kumar Chaki</h3>
+        <p>Age:22</p>
+        <p>DOB:29-Dec-2003</p>
+    </div>
+
+""", unsafe_allow_html=True
+)
+
 st.sidebar.header("Navigation")
 intro = st.sidebar.button("Introduction")
 my_intro = st.sidebar.button("Tanmay Chaki's Introduction")
 project_intro = st.sidebar.button("Project")
 project_aspect = st.sidebar.button("Project Aspect")
+
+
+# st.sidebar.write("**Name:** Tanmay Kumar Chaki")
+# st.sidebar.write("**Age:** 22")
+# st.sidebar.write("**Date of Birth:** 29 Dec 2003")
+
+
+
+st.markdown(
+    """
+<style>
+div.stbutton > button {
+    width: 100%;
+    height: 50px;
+    border_radius: 10px;
+}
+
+</style>
+
+"""
+, unsafe_allow_html=True
+)
+
+st.markdown("""
+
+<style>
+         .fade-in {
+            animation:fadIN 1s ease-in;
+            }   
+            
+         @keyframes fadeIn {
+            from {opacity: 0;} to {opacity: 1;}}
+
+</style>
+
+""", unsafe_allow_html=True)
+
+st.markdown('<div class = "fade-in">Hello</div>', unsafe_allow_html=True)
+
 
 col1, col2 = st.columns(2)
 
@@ -24,3 +78,4 @@ with col1:
 
 with col2:
     st.write("")
+
