69c07d0d55b223dc3c10b2009a8db50b0092d75a
Tanmay Kumar Chaki <chakitanmay45@gmail.com>
2026-04-16 16:20:11 +0530

    commit2

--- webapp.py
+++ webapp.py
@@ -3,5 +3,24 @@ import streamlit as st
 
 st.set_page_config(
     page_title='AI-Driven-Market Transformation',
-    layout="Wide"
+    layout="wide"
 )
+
+#Title
+st.title("AI-Driven Market Transformation: A Comprehensive Analysis")
+
+#Sidebar
+st.sidebar.header("Navigation")
+intro = st.sidebar.button("Introduction")
+my_intro = st.sidebar.button("Tanmay Chaki's Introduction")
+project_intro = st.sidebar.button("Project")
+project_aspect = st.sidebar.button("Project Aspect")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.subheader("Welcome to my Website !")
+    st.write("Hi There !")
+
+with col2:
+    st.write("")
